adc333dd8fc793ebb71eebbcc36dfd27cf9636ec
Carlos A. Blanco <carlosbusinessinquiries@gmail.com>
2022-06-06 10:13:59 -0700

    Fix lintint issue

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,9 @@
-import { Router } from "express";
+import {request, Router} from 'express';
 
 const router = Router();
 
-Router
+router.post('/users', (request, response) => {
+  return response.status(201).send();
+});
+
+export {router};
